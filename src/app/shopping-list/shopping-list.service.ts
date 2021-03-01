cff8bb1080b7422b9766a66631764dac88a692a8
GalynaM <matsygingalyna@gmail.com>
2021-03-01 16:03:46 -0500

    Observables Added

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,8 +1,8 @@
-import { EventEmitter } from '@angular/core';
+import { Subject } from 'rxjs';
 import { Ingredient } from '../shared/ingredient.model';
 
 export class ShoppingListService {
-  ingredientsChange = new EventEmitter<Ingredient[]>();
+  ingredientsChange = new Subject<Ingredient[]>();
   private ingredients: Ingredient[] = [
     new Ingredient('Shripms', 15),
     new Ingredient('Cherry Tomato', 7),
@@ -13,13 +13,13 @@ export class ShoppingListService {
   }
   addIngredients(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
-    this.ingredientsChange.emit(this.ingredients.slice());
+    this.ingredientsChange.next(this.ingredients.slice());
   }
   addIngredient(ingredients: Ingredient[]) {
     // for (let ingredient of ingredients) {
     //   this.addIngredients(ingredient);
     // }
     this.ingredients.push(...ingredients);
-    this.ingredientsChange.emit(this.ingredients.slice());
+    this.ingredientsChange.next(this.ingredients.slice());
   }
 }
