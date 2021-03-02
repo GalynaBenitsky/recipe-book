07127683139d6e57e32f6158d8f12a69236e20ee
GalynaM <matsygingalyna@gmail.com>
2021-03-02 17:02:31 -0500

    Forms, Buttons Edited

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -3,6 +3,7 @@ import { Ingredient } from '../shared/ingredient.model';
 
 export class ShoppingListService {
   ingredientsChange = new Subject<Ingredient[]>();
+  startedEditing = new Subject<number>();
   private ingredients: Ingredient[] = [
     new Ingredient('Shripms', 15),
     new Ingredient('Cherry Tomato', 7),
@@ -10,6 +11,9 @@ export class ShoppingListService {
   ];
   getIngredients() {
     return this.ingredients.slice();
+  }
+  getIngredient(index: number) {
+    return this.ingredients[index];
   }
   addIngredients(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
@@ -20,6 +24,15 @@ export class ShoppingListService {
     //   this.addIngredients(ingredient);
     // }
     this.ingredients.push(...ingredients);
+    this.ingredientsChange.next(this.ingredients.slice());
+  }
+  updateIngerient(index: number, newIngredient: Ingredient) {
+    this.ingredients[index] = newIngredient;
+    this.ingredientsChange.next(this.ingredients.slice());
+  }
+
+  deleteIngredient(index: number) {
+    this.ingredients.splice(index, 1);
     this.ingredientsChange.next(this.ingredients.slice());
   }
 }
