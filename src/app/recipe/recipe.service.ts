07f14eab6da74ba97d2f67ba04a20502a98f7ac0
GalynaM <matsygingalyna@gmail.com>
2021-03-03 16:28:44 -0500

    f

--- src/app/recipe/recipe.service.ts
+++ src/app/recipe/recipe.service.ts
@@ -11,16 +11,6 @@ export class RecipeService {
 
   private recipes: Recipe[] = [
     new Recipe(
-      'Shrimp Pasta',
-      'Shrimp tomato pasta',
-      'https://images.unsplash.com/photo-1563379926898-05f4575a45d8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
-      [
-        new Ingredient('Shripms', 15),
-        new Ingredient('Cherry Tomato', 7),
-        new Ingredient('Pasta', 100),
-      ]
-    ),
-    new Recipe(
       'Salad bowl',
       'Vegan Salad',
       'https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
