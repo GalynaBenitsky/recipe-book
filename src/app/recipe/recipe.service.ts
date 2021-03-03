31efb753d26257bed13af24ba1efae4eeae3a77c
GalynaM <matsygingalyna@gmail.com>
2021-03-03 13:45:54 -0500

    visual part changed

--- src/app/recipe/recipe.service.ts
+++ src/app/recipe/recipe.service.ts
@@ -20,10 +20,34 @@ export class RecipeService {
       ]
     ),
     new Recipe(
+      'Salad bowl',
+      'Vegan Salad',
+      'https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+      [
+        new Ingredient('Avocado', 0.5),
+        new Ingredient('Cherry Tomamto', 100),
+        new Ingredient('Chickpea', 50),
+        new Ingredient('Sweet Potato', 100),
+        new Ingredient('Radish', 20),
+        new Ingredient('Letuce', 70),
+      ]
+    ),
+
+    new Recipe(
       'Cheese Pizza',
       'Pizza with extra cheese',
       'https://images.unsplash.com/photo-1513104890138-7c749659a591?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
       [new Ingredient('Chesse', 300), new Ingredient('Dough', 100)]
+    ),
+    new Recipe(
+      'Cherry Ice Cream',
+      'Homemade cherry ice cream',
+      'https://images.unsplash.com/photo-1517093157656-b9eccef91cb1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80',
+      [
+        new Ingredient('Cherries', 200),
+        new Ingredient('Cream', 150),
+        new Ingredient('Sugar', 50),
+      ]
     ),
   ];
   constructor(private slService: ShoppingListService) {}
