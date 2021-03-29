849207f741c45a1feaf6318154e9bcf771c80980
GalynaM <matsygingalyna@gmail.com>
2021-03-29 11:16:07 -0400

    State

--- src/app/shopping-list/store/shopping-list.reducer.ts
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -1,6 +1,6 @@
 import { Action } from '@ngrx/store';
 import { Ingredient } from '../../shared/ingredient.model';
-import { ADD_INGREDIENT } from './shopping-list.action';
+import * as ShoppingListAction from './shopping-list.action';
 
 const initialState = {
   ingredients: [
@@ -10,9 +10,14 @@ const initialState = {
   ],
 };
 
-export function shoppingListReducer(state = initialState, action: Action) {
+export function shoppingListReducer(
+  state = initialState,
+  action: ShoppingListAction.AddIngredient
+) {
   switch (action.type) {
-    case ADD_INGREDIENT:
-      return { ...state, ingredients: [...state.ingredients, action] };
+    case ShoppingListAction.ADD_INGREDIENT:
+      return { ...state, ingredients: [...state.ingredients, action.payload] };
+    default:
+      return state;
   }
 }
